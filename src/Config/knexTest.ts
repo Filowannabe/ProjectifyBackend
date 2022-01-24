98db3ce387f3bfe6395cc87d2c52e10ec60aa53b
Felipe <felipe.corredor.1249@eam.edu.co>
2022-01-24 12:51:08 -0500

    Connection format knex

--- src/Config/knexTest.ts
+++ src/Config/knexTest.ts
@@ -11,10 +11,11 @@ const TestConfig: IKnexConfig = {
     test: {
         client: 'pg',
         connection: {
-            connectionString: process.env.DB_NAME_TEST,
-            ssl: {
-                rejectUnauthorized: false
-            }
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME_TEST,
         },
         pool: { min: 1, max: 2 },
         migrations: {
